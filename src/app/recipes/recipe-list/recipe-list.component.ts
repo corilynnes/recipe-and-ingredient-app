4ce6dd7422dd2e73945fc5603ebb2fa4cee606e3
cori <coristeiner27@gmail.com>
2020-12-19 15:37:27 -0600

    Added recipe select

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] = [
-  new Recipe('A test', 'just a test', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=1920%2C1080&ssl=1')
-];
+  new Recipe('A test', 'just a test', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=1920%2C1080&ssl=1'),
+  new Recipe('Yum', 'Yum Yum', 'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made.jpg')
+];       
 
   constructor() { }
-
+ 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe)  {
+this.recipeWasSelected.emit(recipe);
+  }
 }
  
