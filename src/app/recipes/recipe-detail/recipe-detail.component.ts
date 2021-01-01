3a717074450a143c1dd2f38174d3aff4f3ca8f07
cori <coristeiner27@gmail.com>
2021-01-01 12:33:20 -0600

    can now add items to shopping list from recipes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,10 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-@Input() recipe: Recipe;
-  constructor() { }
+  @Input() recipe: Recipe;
+  constructor(private reciepeService: RecipeService) { }
 
   ngOnInit(): void {
   }
+  onAddToShoppingList() {
+    this.reciepeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    }
+
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,32 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
+export class RecipeService {
 
-export class RecipeService{
-
-    recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-        new Recipe('A test', 'just a test', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=1920%2C1080&ssl=1'),
-        new Recipe('Yum', 'Yum Yum', 'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made.jpg')
-      ];       
+    new Recipe('Meat Thing', 'just a test', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=1920%2C1080&ssl=1',
+      [
+        new Ingredient('Meat', 1), new Ingredient('Freanch Fries', 20),
+      ]),
+    new Recipe('Yum Burger Thing', 'Yum Yum', 'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made.jpg',
+      [
+        new Ingredient('Meat', 1), new Ingredient('Buns', 2),
+      ])
+  ];
 
-      getRecipes(){
-          return this.recipes.slice();
-      }
+  constructor(private slService: ShoppingListService) {
+
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
